10dc6b2f8942a0604417ec26e4b5ae944ed73052
Muhammad Hisham <mohmadetsh874@gmail.com>
2024-11-12 11:39:43 -0800

    Add aria label

--- src/component/dessertMenu/AddToCart.js
+++ src/component/dessertMenu/AddToCart.js
@@ -52,12 +52,12 @@ const AddToCart = ({ decreamentFunc, increamentFunc, resetFunc, state }) => {
             <span>Add to Cart</span>
           </button>
           <div className={`add-to-cart_count ${state.purchased ? "isPurchased" : ""}`}>
-            <button className="decreament" onClick={() => updateItemsQuantity()}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z" /></svg>
+            <button aria-label="decreament" className="decreament" onClick={() => updateItemsQuantity()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2" aria-hidden><path fill="#fff" d="M0 .375h10v1.25H0V.375Z" /></svg>
             </button>
             <span>{state.quantity}</span>
-            <button className="increament" onClick={() => addToCart()}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" /></svg>
+            <button aria-label="increament" className="increament" onClick={() => addToCart()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10" aria-hidden><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" /></svg>
             </button>
           </div>
         </div>
